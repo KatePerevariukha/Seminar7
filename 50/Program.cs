840c5204ce3b3dd3fa0d2fe14d14e79e91f3619b
Kate <Kate-pretty@ya.ru>
2022-12-16 16:04:28 +0500

    added files

---
+++ 50/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+int[,] matrix = new int[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
+
+
+Console.Write("Номер строки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Номер столбца: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > matrix.GetLength(0) || y > matrix.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение в массиве: {matrix[x, y]}");
+}
